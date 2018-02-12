450d93d1826f55b234b94672d1b85f8277521e82
scorsi <sylvain.corsini@gmail.com>
2018-02-12 20:03:41 +0100

    Add small Main

--- src/main/kotlin/com/scorsi/Main.kt
+++ src/main/kotlin/com/scorsi/Main.kt
@@ -1,8 +1,16 @@
 package com.scorsi
 
+import com.scorsi.gouttelettes.engine.core.Input
 import com.scorsi.gouttelettes.engine.rendering.Window
+import org.lwjgl.glfw.GLFW.GLFW_KEY_ESCAPE
 
 fun main(vararg arg: String) {
-    val window = Window()
-    while (true) {}
+    val window = Window(800, 600)
+    val input = Input(window.id)
+    while (window.isClosing().not()) {
+        window.update()
+        if (input.isKeyPressed(GLFW_KEY_ESCAPE))
+            window.close()
+    }
+    window.destroy()
 }
